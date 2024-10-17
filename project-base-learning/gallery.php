cc2dd8684b2be845680cb3dab0b81ae137c307c3
unknown <kaungsike>
2024-10-18 03:53:55 +0630

    update

---
+++ project-base-learning/gallery.php
@@ -0,0 +1,23 @@
+<?php include("./template/header.php") ?>
+
+<section class="p-10">
+    <div class="bg-base-200 h-[800px] p-5">
+        <h3 class="text-3xl font-bold text-center mb-5">Gallery</h3>
+        <form enctype="multipart/form-data" method="post" action="./gallery-process.php" class="w-full">
+            <div class="flex items-center justify-center gap-5">
+            <input multiple name="photoes[]" type="file" class="file-input file-input-bordered w-full " />
+            </div>
+
+            <button class="btn w-full btn-neutral mt-5">Submit</button>
+
+        </form>
+        <div>
+            <?php
+            
+            ?>
+        </div>
+    </div>
+
+</section>
+
+<?php include("./template/footer.php") ?>
